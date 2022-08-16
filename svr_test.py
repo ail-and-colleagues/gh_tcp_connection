b1b6ba1e548c2b3dbba21eee387b2a53af709029
kkado <k.kado.gm@gmail.com>
2022-08-16 19:24:31 +0900

    operation confirmed ad a sample.

---
+++ svr_test.py
@@ -0,0 +1,68 @@
+from ctypes import sizeof
+import struct
+import sys
+import socket
+import datetime
+import numpy as np
+from struct import pack, unpack
+import datetime
+
+size_of_float = 4
+
+class TCPSvr():
+    def __init__(self, cfg):
+        self.addr = cfg["addr"]
+        self.port = cfg["port"]
+        self.wait = cfg["wait"]
+        self.strc = struct.Struct(cfg["format"])
+        self.sock = None
+        
+
+    def Open(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.addr, self.port))
+        self.sock.settimeout(self.wait)
+        self.sock.listen(5)
+        return
+
+    def ReceiveData(self, func):
+        while 1:
+            print(datetime.datetime.now())
+            try:
+                clientsocket, address = self.sock.accept()
+                print("Connection from {} has been established!".format(address))
+                res = clientsocket.recv(self.strc.size)
+                data = unpack(self.strc.format, res)
+                ret = func(data)
+                print("ret: ", type(ret), ret)
+                data = pack(str(ret.shape[0]) + "f", *ret)
+
+                clientsocket.send(data)
+                clientsocket.close()
+            except socket.timeout:
+                pass
+        
+class Callback():
+    """
+    a class for defining a callback function executed when the server gets data.  
+    """
+    def __init__(self):
+        pass
+    def func(self, arg):
+        # print("type(arg): ", type(arg))
+        arg = np.array(arg)
+        # print("arg.shape: ", arg.shape)
+        arg = arg.reshape([-1, 3])
+        return np.mean(arg, axis=0)
+
+if __name__ == "__main__":
+    cfg = {
+        "addr": "127.0.0.1",
+        "port": 3141,
+        "wait": 1,
+        "format": "30f",
+    }
+    tcpSvr = TCPSvr(cfg)
+    tcpSvr.Open()
+    c = Callback()
+    tcpSvr.ReceiveData(c.func)
--- tcp_connnection.cs
+++ tcp_connnection.cs
@@ -4,6 +4,42 @@ using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
 
+using System.Net.Sockets;
+
+public class TCPCli
+{
+    public string stat;
+    TcpClient client;
+    String server;
+    Int32 port, timeout;
+    public TCPCli(String server, Int32 port, Int32 timeout)
+    {
+        this.server = server;
+        this.port = port;
+        this.timeout = timeout;
+        this.client = null;
+    }
+    public bool Communicate(in byte[] sendData, ref byte[] rcvData)
+    {
+        try
+        {
+            this.client = new TcpClient(this.server, this.port);
+            NetworkStream ns = this.client.GetStream();
+            // send data to the Svr.
+            ns.Write(sendData, 0, sendData.Length);
+            // receive data from the Svr.
+            ns.Read(rcvData, 0, rcvData.Length);
+            this.stat = "OK";
+            return true;
+        }
+        catch (Exception ex)
+        {
+            this.stat = ex.ToString();
+            return false;
+        }
+    }
+}
+
 namespace gh_tcp_conn
 {
     public class tcp_connnection : GH_Component
@@ -27,7 +63,10 @@ namespace gh_tcp_conn
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddTextParameter("test text", "test", "text inputs for test", GH_ParamAccess.item, "this is a test");
+            pManager.AddTextParameter("ipAddress", "addr", "ip addtess of the svr", GH_ParamAccess.item, "127.0.0.1");
+            pManager.AddIntegerParameter("port", "port", "port of the svr", GH_ParamAccess.item, 3141);
+            pManager.AddBooleanParameter("run", "run", "pass True to run", GH_ParamAccess.item, false);
+            pManager.AddPointParameter("points", "pts", "points for solve", GH_ParamAccess.list, null);
         }
 
         /// <summary>
@@ -35,7 +74,10 @@ namespace gh_tcp_conn
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddTextParameter("test text", "test", "text inputs for test", GH_ParamAccess.item);
+            pManager.AddTextParameter("Connection status", "c_stat", "connection status of the Svr", GH_ParamAccess.item);
+            pManager.AddPointParameter("Returned point", "p", "Returned point", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Returned score", "s", "Returned score", GH_ParamAccess.item);
+
         }
 
         /// <summary>
@@ -45,10 +87,53 @@ namespace gh_tcp_conn
         /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            String t = "this is a test";
-            if (!DA.GetData(0, ref t)) return;
+            DA.SetData(0, "chk");
+            string addr = "";
+            if (!DA.GetData(0, ref addr)) return;
+            int port = 0;
+            if (!DA.GetData(1, ref port)) return;
+            bool run = false;
+            if (!DA.GetData(2, ref run) || !run) return;
 
-            DA.SetData(0, "__" + t);
+            List<Point3d> points = new List<Point3d>();
+            if (!DA.GetDataList(3, points)) return;
+
+
+            // flatten point list to float[]
+            List<float> t = new List<float>();
+
+            foreach (Point3d p in points)
+            {
+                t.Add((float)p.X);
+                t.Add((float)p.Y);
+                t.Add((float)p.Z);
+            }
+
+            // convert float[] to byte[]
+            var byteAry = new byte[sizeof(float) * 3 * points.Count];
+            Buffer.BlockCopy(t.ToArray(), 0, byteAry, 0, byteAry.Length);
+
+            // prepate byte[] to receive data from the svr.
+            int rcvSize = 3;
+            var rcvByteAry = new byte[sizeof(float) * rcvSize];
+            var rcvData = new float[rcvSize];
+
+            // communicate via tcp
+            TCPCli cli = new TCPCli(addr, port, 1000);
+            if (!cli.Communicate(byteAry, ref rcvByteAry))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "connection err: " + cli.stat);
+                DA.SetData(0, "connection err: " + cli.stat);
+                return;
+            }
+            DA.SetData(0, String.Format("receive {0} bytes from svr", rcvByteAry.Length));
+
+            Buffer.BlockCopy(rcvByteAry, 0, rcvData, 0, rcvByteAry.Length);
+            var returnedPoint = new Point3d(rcvData[0], rcvData[1], rcvData[2]);
+
+            DA.SetData(1, returnedPoint);
+            DA.SetData(2, rcvData[0]);
+
         }
 
         /// <summary>
